c5d3df9029d15b9970ea62b5746ceb5cc8d37012
yDewolf <andrersng@gmail.com>
2025-06-09 08:51:37 -0300

    Start making the database

--- tagged/src/main/java/com/github/ydewolf/Main.java
+++ tagged/src/main/java/com/github/ydewolf/Main.java
@@ -1,35 +1,7 @@
 package com.github.ydewolf;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main {
     public static void main(String[] args) {
-        Connection connection = null;
-        try {
-            // Cria a conexão com o banco de dados
-            connection = DriverManager.getConnection("jdbc:sqlite:base.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // Espera só por 30 segundos para conectar
-
-            // Roda os comandos para o SQLite
-            statement.executeUpdate("DROP TABLE IF EXISTS terminalroot");
-            statement.executeUpdate("CREATE TABLE terminalroot (id INTEGER, name STRING)");
-            statement.executeUpdate("INSERT INTO terminalroot VALUES(1, 'Marcos Oliveira')");
-            statement.executeUpdate("INSERT INTO terminalroot VALUES(2, 'James Gosling')");
-            ResultSet rs = statement.executeQuery("SELECT * FROM terminalroot");
-            while(rs.next()) {
-                // Ler os dados inseridos
-                System.out.println("NOME DO CARA  : " + rs.getString("name"));
-                System.out.println("IDENTIFICAÇÃO : " + rs.getInt("id"));
-        }
-        } catch(SQLException e) {
-            // Se a mensagem de erro for: "out of memory",
-            // Provavelmente erro ao criar(permissão) ou caminho do banco de dados
-            System.err.println(e.getMessage());
-        }
+        
     }   
 }
---
+++ tagged/src/main/java/com/github/ydewolf/classes/internals/utils/DatabaseUtils.java
@@ -0,0 +1,5 @@
+package com.github.ydewolf.classes.internals.utils;
+
+public class DatabaseUtils {
+    
+}
